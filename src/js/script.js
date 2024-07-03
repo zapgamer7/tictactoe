ca6580835a5b5750378fa0de9616a2a578f553bd
CAUAN ABREU DE ALMEIDA <bv3044092@ad.sbv.ifsp.edu.br>
2024-07-02 21:37:37 -0300

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,20 @@
 let vezDoX = true
 
 function getEl(element){
+    return document.querySelector(element);
+}
+
+function getAllEl(element){
     return document.querySelectorAll(element);
 }
 
 function changePiece(position){
-    if(vezDoX){
-        getEl(".game--column")[position].classList.add("cross")
+    let el = getAllEl(".game--column")[position]
+
+    if(!el.classList.contains("cross") && !el.classList.contains("naught")){
+        el.classList.add(vezDoX?"cross":"naught")
+        vezDoX = !vezDoX
+        getEl("#game--turn").innerText = vezDoX?"vez do X":"vez do O"
     }
+    
 }
